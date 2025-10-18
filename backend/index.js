58f49c43993d63cd93e751cee75664a97b305afe
dev-koushal <koushalydv99@gmail.com>
2025-10-18 22:25:21 +0530

    db connection

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config({});
+const app = express();
+
+app.get('/home',(req,res)=>{
+    return res.status(200).json({
+        message : "Coming from backend!!",
+        success : true
+    })
+})
+
+// middleware
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+app.use(cookieParser());
+
+const corsOptions = {
+    origin : 'http://localhost:5173',
+    Credentials : true
+}
+
+app.use(cors(corsOptions));
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log("Server running at port: "+ PORT);
+})

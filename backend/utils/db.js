2b8b9d9648e1c5d3d36391645ca46f974d661f22
dev-koushal <koushalydv99@gmail.com>
2025-11-01 18:36:43 +0530

    dotenv fu

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config();
 const connectDB = async()=>{
     try{
-        // await mongoose.connect(process.env.MONGO_URI);
-        console.log("The mongo is: " + process.env.MONGO_URI);
-        console.log("mongo db connected!!");
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully!! ✅" );
     }catch (error){
-        console.log(error);
+        console.log("There is an error connecting the databse!! ❌",error);
     }
 }
 

4b3366cfb4f02c4d5231482aa4ba3bca18b39200
dev-koushal <koushalydv99@gmail.com>
2025-11-03 20:53:42 +0530

    update user controllers

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,125 @@
+import { User } from "../models/user.model.js";
+import bycrypt from "bcryptjs";
+import jwt from 'jsonwebtoken'
+// Logic to register a user
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing!!",
+        success: false,
+      });
+    }
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User with this email already exists!",
+        success: false,
+      });
+    }
+    const hashedPassword = await bycrypt.hash(password, 10);
+
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+
+    return res.status(201).json({
+        message:"Account created successfully",
+        success : true
+    })
+  } catch (error) {
+    console.log("Error in creating User" +error);
+  }
+};
+
+// Logic to login a user
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "Something is missing!!",
+        success: false,
+      });
+    }
+
+    const user = User.findOne({ email }); //returns the whole block of that related email!!
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User with this email not found!!",
+        success: false,
+      });
+    };
+
+    const isPasswordMatch = await bycrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        message: "Password is incorrect",
+        success: false,
+      });
+    };
+
+    //403 forbbiden or credential lacks
+    if(role != user.role){
+        return res.status(403).json({
+            message : "Invalid Role!!",
+            success : false
+        })
+    };
+
+    const tokenData = {
+        userId : user._id
+    }
+
+    const token = await jwt.sign(tokenData,process.env.TOKEN_SECRET_KEY,{expiresIn:'1d'});
+
+    user = {
+        _id:user._id,
+        fullname:user.fullname,
+        email:user.email,
+        phoneNumber:user.phoneNumber,
+        role:user.role,
+        profile:user.profile
+    }
+    return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000, httpOnly:true , sameSite:'strict'}).json({
+        message : `Welcome back ${user.fullname}`,
+        success: true
+    })
+
+  } catch (error) {
+    console.log("Login Error User" + error);
+  }
+};
+
+
+// Logout Logic
+export const logout = async (req,res)=>{
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"Logged out successfully",
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// Update profile
+
+export const updateProfile = async (req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+}

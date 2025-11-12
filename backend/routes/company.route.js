007382702c248b035d8235c78424ee1fbafbeb1f
dev-koushal <koushalydv99@gmail.com>
2025-11-12 22:28:54 +0530

    add company route

---
+++ backend/routes/company.route.js
@@ -0,0 +1,11 @@
+import express, { Router } from 'express'
+import { getCompany, getCompanyById, registerCompany,updateCompany } from '../controllers/company.controller.js';
+import isAuthenticated from '../middlewares/isAuthenticated.middleware.js';
+const router = Router();
+
+router.route("/register").post(isAuthenticated,registerCompany);
+router.route("/get").get(isAuthenticated,getCompany);
+router.route("/get/:id").get(isAuthenticated,getCompanyById);
+router.route("/update/:id").put(isAuthenticated, updateCompany);
+
+export default router; 

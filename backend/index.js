2610e8e4edc269bb0240a13f1b1b2c63338d7144
dev-koushal <koushalydv99@gmail.com>
2025-11-12 22:27:39 +0530

    company route

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './utils/db.js';
-import userRoute from './routes/user.route.js'
+import userRoute from './routes/user.route.js';
+import companyRoute from './routes/company.route.js'
 dotenv.config({
     path:"./.env"
 });
@@ -33,6 +34,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT;
 
 app.use('/api/v1/user',userRoute);
+app.use('/api/v1/company',companyRoute);
 
 await connectDB();
 {

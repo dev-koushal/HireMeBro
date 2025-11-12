6d665bc7d71574b3c1b0a2a83a08374e16c5eefe
dev-koushal <koushalydv99@gmail.com>
2025-11-12 13:32:08 +0530

    logout route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express, { Router } from 'express'
-import {login, register, updateProfile} from '../controllers/user.controller.js'
+import {login, logout, register, updateProfile} from '../controllers/user.controller.js'
 import isAuthenticated from '../middlewares/isAuthenticated.middleware.js';
 const router = Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/logout").post(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);
 
-export default router;
+export default router; 

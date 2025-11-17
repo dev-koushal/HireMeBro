c70a032a7409a3de0db580d5d78800d1f2ce2b15
dev-koushal <koushalydv99@gmail.com>
2025-11-17 22:38:35 +0530

    minor

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -29,11 +29,13 @@ export const getJob = async (req,res)=>{
         const keyword = req.query.keyword ||  "";
         const query  ={
             $or:[
-                {title : {$regex:keyword,$option:"i"}},
-                {description: {$regex:keyword,$option:"i"}}
+                {title : {$regex:keyword,$options:"i"}},
+                {description: {$regex:keyword,$options:"i"}}
             ]
         };
-        const jobs = await Job.find(query);
+        const jobs = await Job.find(query).populate({
+            path : "company"
+        }).sort({createdAt : -1}); 
 
         if(!jobs){
             return res.status(404).json({

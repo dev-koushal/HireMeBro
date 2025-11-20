afe85bd2048dbbff815eea00ec1de1ea5922498c
dev-koushal <koushalydv99@gmail.com>
2025-11-20 19:13:33 +0530

    absolute nav

---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,76 @@
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { Avatar } from "../ui/avatar";
+import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { LogOut, User2 } from "lucide-react";
+function Navbar() {
+  return (
+    <div className="bg-white">
+      <div className="flex justify-between items-center m-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            HireMe<span className="text-[#F83002]"> Bro</span>
+          </h1>
+        </div>
+        <div className="flex item-center gap-12">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+            {/* <li><Link>Home</Link></li>
+          <li><Link>Jobs</Link></li>
+          <li><Link>Browse</Link></li> */}
+          </ul>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="profile"
+                />
+                <AvatarFallback>Pf</AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent className="w-80 p-4 border-2 shadow-sm">
+              <div className="flex items-center space-y-2 gap-4 ">
+                <Avatar className="cursor-pointer">
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="profile"
+                  />
+                  <AvatarFallback>Pf</AvatarFallback>
+                </Avatar>
+                <div>
+                  <h3 className="font-medium ">Koushal Yadav</h3>
+                  <p className="text-sm text-muted-foreground">
+                    Lorem ipsum dolor sit amet.
+                  </p>
+                </div>
+              </div>
+
+              <div className="mt-4 flex flex-col text-gray-600">
+                <div className="cursor-pointer flex gap-2 items-center w-fit">
+                  <User2/>
+                  <Button variant="link" >View Profile</Button>
+                </div>
+                <div className="cursor-pointer flex gap-2 items-center w-fit">
+                  <LogOut/>
+                <Button variant="link" className="cursor-pointer">Logout</Button>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

718d95cdbc1447d3a5df66c1fabb520ba31f2293
dev-koushal <koushalydv99@gmail.com>
2025-11-24 16:39:35 +0530

    signup api

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -4,11 +4,17 @@ import { Label } from "@radix-ui/react-label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
 import { Button } from "../ui/button";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { AwardIcon } from "lucide-react";
+import axios from "axios";
+import { USER_API_END_POINT } from "@/utils/constant";
+import { toast } from "sonner";
 function Signup() {
   const [input,setInput] = useState({
     fullname:"", email:"", phoneNumber:"", password:"", role:"",file:""
   })
+
+  const navigate = useNavigate();
 
   const changeEventHandler = (e) =>{
     setInput({...input,[e.target.name]:e.target.value});
@@ -20,7 +26,31 @@ function Signup() {
 
   const submitHandler = async(e)=>{
       e.preventDefault();
-      console.log(input);
+      const formData = new FormData();
+      formData.append("fullname",input.fullname);
+      formData.append("email",input.email);
+      formData.append("phoneNumber",input.phoneNumber);
+      formData.append("password",input.password);
+      formData.append("role",input.role);
+      if(input.file){
+        formData.append("file",input.file);
+      }
+     try {
+      const res = await axios.post(`${USER_API_END_POINT}/register`,formData,{
+        headers :{
+            "Content-Type":"multipart/form-data"
+        },
+        withCredentials : true,
+      });
+      if(res.data.success){
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+
+     } catch (error) {
+      // console.log(error);
+      toast.error(error.response.data.message);
+     }
   }
   return (
     <>
@@ -86,7 +116,7 @@ function Signup() {
             </div>
           </div>
 
-          <Button className="w-full my-2">Signup</Button>
+          <Button className="w-full my-2" >Signup</Button>
           <span className="text-sm cursor-pointer">Already have account? <Link className="text-blue-600" to="/login">Login</Link></span>
         </form>
       </div>

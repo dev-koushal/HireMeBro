f75c298e163cc0aa41504ace970b93084871d43e
dev-koushal <koushalydv99@gmail.com>
2025-11-24 16:18:56 +0530

    packages =

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
 import express, { Router } from 'express'
 import {login, logout, register, updateProfile} from '../controllers/user.controller.js'
 import isAuthenticated from '../middlewares/isAuthenticated.middleware.js';
+import { singleUpload } from '../middlewares/multer.js';
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
 router.route("/logout").post(logout);
 router.route("/profile/update").post(isAuthenticated, updateProfile);

58f2e108bfbbdca826229d726ae86a85964c3949
dev-koushal <koushalydv99@gmail.com>
2025-12-01 16:23:37 +0530

    adding

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
+import CatergoryCarousel from './CatergoryCarousel'
+import LatestJobs from './LatestJobs'
 
 function Home() {
   return (
     <div>
         <Navbar/>
         <HeroSection/>
-        
+        <CatergoryCarousel/>
+        <LatestJobs/>
     </div>
     
   )

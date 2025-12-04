3ffc03dc7457686c9e62a82f82657ef0bbe9270d
dev-koushal <koushalydv99@gmail.com>
2025-12-04 10:00:19 +0530

    filtercard

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./shared/Navbar";
 import FilterCard from "./FilterCard";
 import Job from "./Job";
+import { Button } from "./ui/button";
+import { FilterIcon } from "lucide-react";
 
 const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 function Jobs() {
@@ -10,15 +12,15 @@ function Jobs() {
       <Navbar />
       <div className=" max-w-full md:max-w-7xl mt-5 mx-auto">
         <div className="flex gap-5 ">
-          <div className="w-[20%] border-2">
+          <div className="hidden md:block w-0 md:w-[20%] border-2">
             <FilterCard />
           </div>
 
           {jobsArray.length <= 0 ? (
             <span>Job not Found!!</span>
           ) : (
-            <div className="flex-1 h-[88vh] overflow-y-auto pb-5">
-              <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4">
+            <div className="flex-1 md:h-[88vh] overflow-y-auto pb-5">
+              <div className="grid grid-cols-1 md:grid-cols-4 gap-2 md:gap-4">
                 {jobsArray.map((item, index) => (
                   <div>
                     <Job key={index} />

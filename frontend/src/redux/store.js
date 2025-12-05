55e95ff97e393236460098a060c7d90a45621986
dev-koushal <koushalydv99@gmail.com>
2025-12-05 21:26:24 +0530

    fix Redux

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authSlice from './authSlice';
+import authSlice from "@/redux/authSlice";
+
 const store = configureStore({
     reducer:{
         auth : authSlice

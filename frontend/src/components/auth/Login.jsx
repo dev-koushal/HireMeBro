32ba9770763a91541c7db28f002cb4d37e3174d1
dev-koushal <koushalydv99@gmail.com>
2025-12-05 21:26:07 +0530

    Login fix

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -9,8 +9,7 @@ import axios from "axios";
 import { USER_API_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
 import { useDispatch, useSelector } from "react-redux";
-import { setLoading } from "@/redux/authSlice";
-import store from "@/redux/store";
+import { setLoading, setUser } from "@/redux/authSlice";
 import { Loader2 } from "lucide-react";
 
 function Login() {
@@ -39,7 +38,8 @@ function Login() {
         withCredentials : true,
       });
       if(res.data.success){
-        navigate('/');
+        dispatch(setUser(res.data.user))
+        setTimeout(() => navigate('/'), 50);
         toast.success(res.data.message);
       }
 

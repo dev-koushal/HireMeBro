2100ed98112e2de30869855714b90ea9fbfa3137
dev-koushal <koushalydv99@gmail.com>
2025-12-17 20:00:25 +0530

    minor

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.route("/register").post(singleUpload,register);
 router.route("/login").post(login);
-router.route("/logout").post(logout);
+router.route("/logout").get(logout);
 router.route("/profile/update").post(isAuthenticated, singleUpload, updateProfile);
 
 export default router; 

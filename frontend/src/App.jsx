7bf23b05d08f5f6700e4f30abd69ed7ae7bfb4ea
dev-koushal <koushalydv99@gmail.com>
2025-12-17 16:47:40 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,48 @@
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-import Login from './components/auth/Login'
-import Signup from './components/auth/Signup'
-import Home from './components/Home'
-import Jobs from './components/Jobs'
-import Browse from './components/Browse'
-import Profile from './components/Profile'
-// import JobDescription from './components/JobDescription'
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
+import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
+import Profile from "./components/Profile";
+import JobDescription from './components/JobDescription'
 const appRouter = createBrowserRouter([
   {
-    path:'/',
-    element:<Home/>
+    path: "/",
+    element: <Home />,
   },
   {
-    path:'/login',
-    element:<Login/>
+    path: "/login",
+    element: <Login />,
   },
   {
-    path:'/signup',
-    element:<Signup/>
+    path: "/signup",
+    element: <Signup />,
   },
   {
-    path:'/jobs',
-    element:<Jobs/>
-  },
-//   {
-// path:'/description/:id',
-// element:<JobDescription/>
-//   },
-  {
-    path:'/browse',
-    element:<Browse/>
+    path: "/jobs",
+    element: <Jobs />,
   },
   {
-    path : '/profile',
-    element : <Profile/>
-  }
-])
+    path: "/description/:id",
+    element: <JobDescription />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
 function App() {
-
   return (
     <>
-      <RouterProvider router ={appRouter}/> 
+      <RouterProvider router={appRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;

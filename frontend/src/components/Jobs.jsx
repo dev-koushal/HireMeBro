a88d2e7015ff755458483432d83eb158285f71f6
dev-koushal <koushalydv99@gmail.com>
2025-12-21 21:10:21 +0530

    minor

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -4,9 +4,10 @@ import FilterCard from "./FilterCard";
 import Job from "./Job";
 import { Button } from "./ui/button";
 import { FilterIcon } from "lucide-react";
+import { useSelector } from "react-redux";
 
-const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
 function Jobs() {
+  const {allJobs} = useSelector(store=>store.job); 
   return (
     <div>
       <Navbar />
@@ -16,14 +17,14 @@ function Jobs() {
             <FilterCard />
           </div>
 
-          {jobsArray.length <= 0 ? (
+          {allJobs.length <= 0 ? (
             <span>Job not Found!!</span>
           ) : (
             <div className="flex-1 md:h-[88vh]  pb-5">
               <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4">
-                {jobsArray.map((item, index) => (
+                {allJobs.map((job) => (
                   <div>
-                    <Job key={index} />
+                    <Job key={job._id} job={job} />
                   </div>
                 ))}
               </div>

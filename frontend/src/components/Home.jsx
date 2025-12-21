674274d42340ca2ca6a3ccb5da83bc49bdf37fff
dev-koushal <koushalydv99@gmail.com>
2025-12-21 21:10:03 +0530

    minor

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -4,8 +4,10 @@ import HeroSection from './HeroSection'
 import CatergoryCarousel from './CatergoryCarousel'
 import LatestJobs from './LatestJobs'
 import Footer from './Footer'
+import useGetAllJobs from '@/hooks/useGetAllJobs'
 
 function Home() {
+  useGetAllJobs();
   return (
     <div>
         <Navbar/>

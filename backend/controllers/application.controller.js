157a8a7e45a7d8712f9eee4574b89081eb7fc775
dev-koushal <koushalydv99@gmail.com>
2026-01-07 09:34:58 +0530

    minors

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -1,75 +1,67 @@
-import {Application} from '../models/Application.model.js'
-import {Job} from '../models/job.model.js'
-export const applyJob = async (req,res)=>{
+import { Application } from "../models/application.model.js";
+import { Job } from "../models/job.model.js";
+
+export const applyJob = async (req, res) => {
     try {
         const userId = req.id;
         const jobId = req.params.id;
-        if(!jobId){
+        if (!jobId) {
             return res.status(400).json({
-                message : " Id is required",
-                success : false
+                message: "Job id is required.",
+                success: false
             })
         };
+        // check if the user has already applied for the job
+        const existingApplication = await Application.findOne({ job: jobId, applicant: userId });
 
-    
-        // check if user is already applied to the job
-
-        const existingApplication = await Application.findOne({job:jobId,applicant:userId});
-
-        if(existingApplication){
+        if (existingApplication) {
             return res.status(400).json({
-                message : "You have already applied for this job!!",
-                success : false
-            })
+                message: "You have already applied for this jobs",
+                success: false
+            });
         }
-        
-        // check if job exist
+
+        // check if the jobs exists
         const job = await Job.findById(jobId);
-        if(!job){
-            return res.status(400).json({
-                message : "Job does not exist!",
-                success : false
+        if (!job) {
+            return res.status(404).json({
+                message: "Job not found",
+                success: false
             })
         }
-
         // create a new application
-        
         const newApplication = await Application.create({
             job:jobId,
-            applicant : userId
-        })
+            applicant:userId,
+        });
 
-        
         job.applications.push(newApplication._id);
         await job.save();
         return res.status(201).json({
-            message : "Job applied succefully!!",
-            success : true
+            message:"Job applied successfully.",
+            success:true
         })
     } catch (error) {
         console.log(error);
     }
-}; 
-
-export const getAppliedJobs = async(req,res)=>{
+};
+export const getAppliedJobs = async (req,res) => {
     try {
         const userId = req.id;
-        const application = await Application.find({applicant:userId}).sort({createdAt: -1}).populate({
-            path : 'job',
+        const application = await Application.find({applicant:userId}).sort({createdAt:-1}).populate({
+            path:'job',
             options:{sort:{createdAt:-1}},
             populate:{
-                path : "company",
-                options:{sort:{createdAt:-1}}, 
+                path:'company',
+                options:{sort:{createdAt:-1}},
             }
         });
-
         if(!application){
             return res.status(404).json({
-                message : "No application",
-                success : false
+                message:"No Applications",
+                success:false
             })
-        }
-
+        };
         return res.status(200).json({
             application,
             success:true
@@ -78,67 +70,60 @@ export const getAppliedJobs = async(req,res)=>{
         console.log(error);
     }
 }
-
-// admin can see the user applied to the job
-export const getApplicants = async (req,res)=>{
+// admin dekhega kitna user ne apply kiya hai
+export const getApplicants = async (req,res) => {
     try {
-        const jobId =req.params.id;
-
+        const jobId = req.params.id;
         const job = await Job.findById(jobId).populate({
-            path:"applications",
-            options : {sort:{createdAt:-1}},
+            path:'applications',
+            options:{sort:{createdAt:-1}},
             populate:{
-                path:"applicant"
+                path:'applicant'
             }
         });
         if(!job){
             return res.status(404).json({
-                message : "No Job found",
-                success : false
+                message:'Job not found.',
+                success:false
             })
         };
-
         return res.status(200).json({
-            job,
-            success:true
-        })
+            job, 
+            succees:true
+        });
     } catch (error) {
         console.log(error);
     }
 }
-
-
-// I needa add a status of the applicant 
-export const updateStatus = async (req,res)=>{
+export const updateStatus = async (req,res) => {
     try {
         const {status} = req.body;
-        const applicantionId =req.params.id;
-        
+        const applicationId = req.params.id;
         if(!status){
-             return res.status(400).json({
-                message : "status is require",
-                success : false
+            return res.status(400).json({
+                message:'status is required',
+                success:false
             })
-        }
+        };
 
-        const application = await Application.findOne({_id:applicantionId})
-
-         if(!application){
-             return res.status(400).json({
-                message : "Application not found",
-                success : false
+        // find the application by applicantion id
+        const application = await Application.findOne({_id:applicationId});
+        if(!application){
+            return res.status(404).json({
+                message:"Application not found.",
+                success:false
             })
-        }
+        };
 
-        // updating would be great in the status
-
+        // update the status
         application.status = status.toLowerCase();
         await application.save();
 
         return res.status(200).json({
-            message : "status updated",
-            success : true
-        })
+            message:"Status updated successfully.",
+            success:true
+        });
+
     } catch (error) {
         console.log(error);
     }

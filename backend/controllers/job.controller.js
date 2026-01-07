c565ac7ef21222b1e4b7f56544468a5af74dfd79
dev-koushal <koushalydv99@gmail.com>
2026-01-07 09:35:03 +0530

    minors

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -33,61 +33,59 @@ export const getJob = async (req,res)=>{
                 {description: {$regex:keyword,$options:"i"}}
             ]
         };
-        const jobs = await Job.find(query).populate({
-            path : "company"
-        }).sort({createdAt : -1}); 
-
-        if(!jobs){
+         const jobs = await Job.find(query).populate({
+            path: "company"
+        }).sort({ createdAt: -1 });
+        if (!jobs) {
             return res.status(404).json({
-                message:"Job not found",
-                success : false
+                message: "Jobs not found.",
+                success: false
             })
-        }
+        };
         return res.status(200).json({
             jobs,
-            success:true
+            success: true
         })
     } catch (error) {
         console.log(error);
     }
 }
 
-export const getJobById = async (req,res) =>{
+export const getJobById = async (req, res) => {
     try {
         const jobId = req.params.id;
-        const job = await Job.findById(jobId);
-        if(!job){
-             return res.status(404).json({
-                message:"Job not found",
-                success : false
+        const job = await Job.findById(jobId).populate({
+            path:"applications"
+        });
+        if (!job) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
             })
         };
-
-         return res.status(200).json({
-                job,
-                success :true
-            })
+        return res.status(200).json({ job, success: true });
     } catch (error) {
         console.log(error);
     }
 }
 
-export const getAdminJobs = async (req,res)=>{
+export const getAdminJobs = async (req, res) => {
     try {
         const adminId = req.id;
-        
-        const jobs = await Job.find({created_by:adminId})
-
-        if(!jobs){
-             return res.status(404).json({
-                message:"Job not found",
-                success : false
+        const jobs = await Job.find({ created_by: adminId }).populate({
+            path:'company',
+            createdAt:-1
+        });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
             })
         };
-         return res.status(200).json({
-                jobs,
-                success :true
-            })
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
     } catch (error) {
         console.log(error);
     }

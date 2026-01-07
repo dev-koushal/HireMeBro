e6c1e3220964967149ab9ef71757f6c79f8c6814
dev-koushal <koushalydv99@gmail.com>
2026-01-07 09:35:08 +0530

    minors

--- frontend/src/components/JobDescription.jsx
+++ frontend/src/components/JobDescription.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
@@ -15,9 +15,25 @@ function JobDescription() {
   const params = useParams();
   const {singleJob} = useSelector(store=>store.job);
   const {user} = useSelector(store=>store.auth);
+  const isIntiallyApplied = singleJob?.applications?.some(application => application.applicant === user?._id) || false;
+  const [isApplied, setIsApplied] = useState(isIntiallyApplied);
   const jobId = params.id;
-  const isApplied = singleJob?.applications?.some(application=>application.applicant === user?._id) || false;
-  
+   const applyJobHandler = async () => {
+        try {
+            const res = await axios.get(`${APPLICATION_API_END_POINT}/apply/${jobId}`, {withCredentials:true});
+            
+            if(res.data.success){
+                setIsApplied(true); // Update the local state
+                const updatedSingleJob = {...singleJob, applications:[...singleJob.applications,{applicant:user?._id}]}
+                dispatch(setSingleJob(updatedSingleJob)); // helps us to real time UI update
+                toast.success(res.data.message);
+
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        }
+    }
    useEffect(()=>{
        const fetchSingleJob = async ()=>{
          try {
@@ -59,14 +75,10 @@ function JobDescription() {
             </div>
           </div>
           <Button
-            disabled={isApplied}
-            className={`rounded-lg ${
-              isApplied
-                ? "bg-gray-700 cursor-not-allowed"
-                : "bg-blue-800 hover:bg-blue-900 cursor-pointer"
-            }`}
-          >
-            {isApplied ? "Applied" : "Apply now"}
+            onClick={isApplied ? null : applyJobHandler}
+                    disabled={isApplied}
+                    className={`rounded-lg ${isApplied ? 'bg-gray-600 cursor-not-allowed' : 'bg-[#7209b7] hover:bg-[#5f32ad]'}`}>
+                    {isApplied ? 'Already Applied' : 'Apply Now'}
           </Button>
         </div>
         <div className="my-4 space-y-2">
